712d9507b7e2d2b9dc756fb3ddabd4eb3b0fee15
Josema996 <101608821+Josema996@users.noreply.github.com>
2023-07-11 17:45:20 -0300

    e-commerce

---
+++ client/js/products.js
@@ -0,0 +1,37 @@
+const productos = [
+    {
+        id: 1,
+        productName: "Ojo de dios",
+        price: 10,
+        quanty: 1,
+        img: "./images/chandra1.png",
+    },
+    {
+        id: 2,
+        productName: "Mano de la cordura",
+        price: 20,
+        quanty: 1,
+        img: "./images/chandra2.png",
+    },
+    {
+        id: 3,
+        productName: "Luz tenue",
+        price: 30,
+        quanty: 1,
+        img: "./images/chandra3.png",
+    },
+    {
+        id: 4,
+        productName: "Poder de la selva",
+        price: 70,
+        quanty: 1,
+        img: "./images/chandra4.png",
+    },
+    {
+        id: 5,
+        productName: "Ira de dios",
+        price: 50,
+        quanty: 1,
+        img: "./images/chandra5.png",
+    },
+];
